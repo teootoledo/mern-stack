996ec5baaaca5e9d02221709593e64ba9f04add0
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-16 12:24:02 -0300

    Setup de cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Settings ======================================
@@ -6,8 +7,12 @@ const app = express();
 // Si no existe PORT definida en las variables de entorno utiliza por defecto el 4000
 app.set("port", process.env.PORT || 4000);
 
-// Middlewares
+// Middlewares ===================================
+app.use(cors());
+app.use(express.json());
 
 // Routes
+app.get("/api/users", (req, res) => res.send("Users Routes"));
+app.get("/api/notes", (req, res) => res.send("Notes Routes"));
 
 module.exports = app;
