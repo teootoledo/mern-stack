113064ff4d366a1614919d90588d85e34218ac59
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-19 10:43:08 -0300

    Creados Schema y Model de User y Note

---
+++ backend/src/models/Note.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const noteSchema = new Schema(
+  {
+    title: String,
+    content: {
+      type: String,
+      required: true,
+    },
+    author: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Note", noteSchema);
