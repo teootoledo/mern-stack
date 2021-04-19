53742761e76bcdcb53f39d3d0addf86867cb2f7e
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-19 14:17:47 -0300

    Agrego React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Navigation from "./components/Navigation";
 import NotesList from "./components/NotesList";
 import CreateNote from "./components/CreateNote";
@@ -5,13 +8,14 @@ import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
-      Hello world
-      <NotesList />
-      <CreateNote />
-      <CreateUser />
-    </div>
+
+      <Route path="/" exact component={NotesList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
