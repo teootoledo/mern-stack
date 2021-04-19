f11e05f7c67bb719b5e4b8b78b41e92d813dca3c
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-19 14:29:58 -0300

    Agregado bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 import Navigation from "./components/Navigation";
 import NotesList from "./components/NotesList";
 import CreateNote from "./components/CreateNote";
