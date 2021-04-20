090d9cc8498a4e8d4e8898cf02f41b4e4afab43b
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-20 13:45:50 -0300

    Implemento guardado de notas en BD

--- frontend/src/components/CreateNote/index.js
+++ frontend/src/components/CreateNote/index.js
@@ -16,12 +16,22 @@ export default class CreateNote extends Component {
 
   async componentDidMount() {
     const res = await axios.get("http://localhost:4000/api/users");
-    this.setState({ users: res.data.map((user) => user.username) });
+    this.setState({
+      users: res.data.map((user) => user.username),
+      userSelected: res.data[0].username,
+    });
   }
 
-  onSubmit = (e) => {
-    console.log(this.state.title, this.state.content);
+  onSubmit = async (e) => {
     e.preventDefault();
+    const newNote = {
+      title: this.state.title,
+      content: this.state.content,
+      date: this.state.date,
+      author: this.state.userSelected,
+    };
+    const res = await axios.post("http://localhost:4000/api/notes", newNote);
+    console.log(res);
   };
 
   onInputChange = (e) => {
