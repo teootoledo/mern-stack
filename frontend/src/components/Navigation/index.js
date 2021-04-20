f0304dd409e7420bc262a41c935932c8810e1f86
Teo Martin Toledo <49320625+teootoledo@users.noreply.github.com>
2021-04-20 09:24:57 -0300

    Configurado Navigation

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            NotesApp
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href="/create"
-                >
-                  Create note
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/edit/">
-                  Edit note
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/user">
-                  Users
-                </a>
-              </li>
-            </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              NotesApp
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">
+                    Notes
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/create">
+                    Create Note
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/user">
+                    Create User
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
